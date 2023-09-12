3a705b752a49ce17cf9c219e987f640e39e85dd5
Zainab Omar <zainab.alshaikhli01@gmail.com>
2023-09-12 15:10:18 -0500

    commit tree 7005bd5b46de7551059253bc4ea1f4922a2ea52d

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,5 +2,19 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
+  def city_openings(date1, date2)
+    city = City.all
+    openings = []
+    city.each do |listing|
+      listing.listings.each do |list|
+        if (list.created_at.strftime("%Y-%m-%d").between?(date1, date2))
+          openings << list
+        end
+      end
+    end
+    return openings
+  end
+
 end
 
+#  City.first.listings[1].created_at.strftime("%Y-%m-%d").between?("2023-09-10", "2023-09-13")
